888e975605bf6f6bb8b92c4e257f2d1f7d40b2b9
Karacraft <karacraft@gmail.com>
2021-04-05 19:39:06 +0500

    Added Nodejs Support

--- setup.sh
+++ setup.sh
@@ -24,4 +24,10 @@ php artisan config:clear  && \
 php artisan view:clear  && \
 php artisan config:cache
 
+echo "Now we will install Node"
+curl -sL https://deb.nodesource.com/setup_14.x -o nodesource_setup.sh && \
+bash nodesource_setup.sh
+apt install nodejs
+npm install -g npm-check-updates
+echo "Node Installed - Check via node -v or npm -v"
 echo "Done..."
